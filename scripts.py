c0e8177314500287b1be0e49614bf8906678c037
Stanislav <77130336+standbit@users.noreply.github.com>
2022-02-25 23:16:37 +0300

    Create special function for exceptions

--- scripts.py
+++ scripts.py
@@ -1,10 +1,16 @@
-def fix_marks(child_name):
+def catch_exception(child_name):
     try:
         child = Schoolkid.objects.get(full_name__contains=child_name)
+        pass
     except Schoolkid.DoesNotExist:
-        print("Нет ученика с таким именем и фамилией")
+        print("Нет ученика с таким именем и фамилией.")
     except Schoolkid.MultipleObjectsReturned:
         print("Найдено несколько учеников с введенными данными.")
+    return child
+
+
+def fix_marks(child_name):
+    child = catch_exception(child_name)
     bad_marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
     for mark in bad_marks:
         mark.points = 5
@@ -13,12 +19,7 @@ def fix_marks(child_name):
 
 
 def delete_chastisements(child_name):
-    try:
-        child = Schoolkid.objects.get(full_name__contains=child_name)
-    except Schoolkid.DoesNotExist:
-        print("Нет ученика с таким именем и фамилией")
-    except Schoolkid.MultipleObjectsReturned:
-        print("Найдено несколько учеников с введенными данными.")
+    child = catch_exception(child_name)
     chastisements = Chastisement.objects.filter(schoolkid=child)
     for chastisement in chastisements:
         chastisement.delete()
@@ -26,12 +27,16 @@ def delete_chastisements(child_name):
 
 
 def create_commendation(child_name, subject_title):
+    child = catch_exception(child_name)
     try:
-        child = Schoolkid.objects.get(full_name__contains=child_name)
         subjects = Lesson.objects.filter(
             year_of_study=child.year_of_study,
             group_letter=child.group_letter,
             subject__title=subject_title)
+        subjects.get()
+    except Lesson.DoesNotExist:
+        print("Нет такого предмета. Проверь название.")
+    except Lesson.MultipleObjectsReturned:
         commendation = random.choice(commendations)
         subject = subjects[random.randint(0, len(subjects))]
         Commendation.objects.create(
@@ -40,8 +45,4 @@ def create_commendation(child_name, subject_title):
             teacher=subject.teacher,
             text=commendation,
             created=subject.date)
-    except Schoolkid.DoesNotExist or Lesson.DoesNotExist:
-        print("Нет ученика или предмета с таким именем.")
-    except Schoolkid.MultipleObjectsReturned:
-        print("Найдено несколько учеников с введенными данными.")
     return
