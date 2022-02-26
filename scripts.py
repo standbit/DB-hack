26247dbd311093905e4a05ee6618566f136f7e6a
Stanislav <77130336+standbit@users.noreply.github.com>
2022-02-26 11:55:38 +0300

    Rewrite ORM query

--- scripts.py
+++ scripts.py
@@ -28,21 +28,22 @@ def delete_chastisements(child_name):
 
 def create_commendation(child_name, subject_title):
     child = catch_exception(child_name)
-    try:
-        subjects = Lesson.objects.filter(
-            year_of_study=child.year_of_study,
-            group_letter=child.group_letter,
-            subject__title=subject_title)
-        subjects.get()
-    except Lesson.DoesNotExist:
-        print("Нет такого предмета. Проверь название.")
-    except Lesson.MultipleObjectsReturned:
+    subjects = Lesson.objects.filter(
+        year_of_study=child.year_of_study,
+        group_letter=child.group_letter,
+        subject__title=subject_title).only(
+            "subject",
+            "teacher",
+            "date").order_by('?')
+    if subjects.exists():
+        subject = subjects.last()
         commendation = random.choice(commendations)
-        subject = subjects[random.randint(0, len(subjects))]
         Commendation.objects.create(
             schoolkid=child,
             subject=subject.subject,
             teacher=subject.teacher,
             text=commendation,
             created=subject.date)
+    else:
+        print("Нет такого предмета. Проверьте его название.")
     return
